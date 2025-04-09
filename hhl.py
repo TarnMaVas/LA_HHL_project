9182106917c6670157de3e3649a7f5c6344657f7
Maxim-Vasyl Tarnavskyi <tarnmavas@gmail.com>
2025-04-09 13:42:01 +0300

    Measurement

--- hhl.py
+++ hhl.py
@@ -92,7 +92,7 @@ def add_controlled_rotations(circuit, c_req, a_reg, eigvals, C):
         circuit.append(rotation, control_qubits + [a_reg[0]])
 
 
-def HHL(A, b, draw_qc = False):
+def HHL_sim(A, b, draw_qc = False):
     n_c, norm_eigh = estimate_c_reg_size(A)
     n_b = ceil(np.log2(len(b)))
 
@@ -152,9 +152,78 @@ def HHL(A, b, draw_qc = False):
     return curr_state, probabilities
 
 
+def HHL(A, b, draw_qc = False):
+    n_c, norm_eigh = estimate_c_reg_size(A)
+    n_b = ceil(np.log2(len(b)))
+
+    t = 3 * pi / 4
+
+    c_reg = QuantumRegister(n_c, "c")
+    b_reg = QuantumRegister(n_b, "b")
+    a_reg = QuantumRegister(1, "a")
+    clas = ClassicalRegister(n_b + 1, "classical")
+
+    qc = QuantumCircuit(a_reg, c_reg, b_reg, clas)
+
+    prepare_register(qc, b, b_reg)
+
+    qc.barrier()
+
+    for qubit in c_reg:
+        qc.h(qubit)
+
+    qc.barrier()
+
+    eiAt(qc, c_reg, b_reg, A, t)
+
+    qc.barrier()
+
+    qft_circuit = QuantumCircuit(c_reg)
+    qft(qft_circuit)
+    inverse_qft = qft_circuit.inverse()
+    qc.compose(inverse_qft, qubits=c_reg, inplace=True)
+
+    qc.barrier()
+    
+    add_controlled_rotations(qc, c_reg, a_reg, norm_eigh, 1)
+
+    qc.barrier()
+
+    qc.compose(qft_circuit, qubits=c_reg, inplace=True)
+
+    qc.barrier()
+
+    reverse_eiAt(qc, c_reg, b_reg, A, t)
+
+    qc.barrier()
+
+    for qubit in c_reg:
+        qc.h(qubit)
+
+    qc.barrier()
+
+    qc.measure(a_reg[0], clas[0])
+    for i in range(n_b):
+        qc.measure(b_reg[i], clas[i + 1])
+
+    backend = Aer.get_backend('qasm_simulator')
+    transpiled_qc = transpile(qc, backend)
+    result = backend.run(transpiled_qc, shots=1024).result()
+    counts = result.get_counts()
+
+    filtered_counts = {key[:-1]: val for key, val in counts.items() if key[-1] == '1'}
+
+    if draw_qc:
+        qc.draw("mpl")
+
+    return counts, filtered_counts
+
 if __name__ == "__main__":
     A = np.array([[1, -1/3], [-1/3, 1]])
     b = np.array([0, 1])
 
-    meow, lol = HHL(A, b, True)
-    print(lol)
+    # meow, lol = HHL_sim(A, b, True)
+    # print(lol)
+
+    _, filtered_counts = HHL(A, b, True)
+    print(filtered_counts)
